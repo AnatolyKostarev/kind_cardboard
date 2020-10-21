e3b764d3d081584acbccdb10756d1f1bb55ff396
Anatoly Kostarev <anatolykostarev@gmail.com>
2020-10-21 16:02:22 +0300

    HW 1.5 - подключена сетка bootstrap, модальное окно popup

--- functions.php
+++ functions.php
@@ -5,4 +5,6 @@
 		wp_enqueue_style( 'animate', get_template_directory_uri(  ) . './assets/js/animate.css' );
 		wp_enqueue_style( 'slick', get_template_directory_uri(  ) . './assets/slick/slick.css' );
 		wp_enqueue_style( 'slick_theme', get_template_directory_uri(  ) . './assets/slick/slick-theme.css' );
+		wp_enqueue_style( 'bootstrap', get_template_directory_uri(  ) . './assets/css/bootstrap-grid.min.css' );
+		wp_enqueue_style( 'popup', get_template_directory_uri(  ) . './assets/css/magnific-popup.css' );
 	}
