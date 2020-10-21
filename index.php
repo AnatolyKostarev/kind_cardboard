8582c5d0c166fd5c5942e0869398b4121b06502a
Anatoly Kostarev <anatolykostarev@gmail.com>
2020-10-21 15:50:36 +0300

    Homework 1.2 - закоментарен файл анимации animate.css

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Добрый картон</title>
     <link rel="shortcut icon" href="img/Header/header-logo-320.png" type="image/x-icon">
-    <link rel="stylesheet" href="js/animate.css">
+    <!-- <link rel="stylesheet" href="js/animate.css"> -->
     <link rel="stylesheet" href="slick/slick.css">
     <link rel="stylesheet" href="slick/slick-theme.css">
     <link rel="stylesheet" href="css/bootstrap-grid.min.css">
