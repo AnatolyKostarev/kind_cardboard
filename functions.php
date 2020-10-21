3370e5e80466eee7f8d01e2b1d185bee9753ec34
Anatoly Kostarev <anatolykostarev@gmail.com>
2020-10-21 15:49:43 +0300

    Homework 1.2 - подключен анимация animate.css

--- functions.php
+++ functions.php
@@ -2,4 +2,5 @@
 	add_action ( 'wp_enqueque_scripts', 'theme_name_scripts');
 	function theme_name_scripts() {
 		wp_enqueue_style( 'style', get_stylesheet_uri(  ) );
+		wp_enqueue_style( 'animate', get_template_directory_uri(  ) . './assets/js/animate.css' );
 	}
