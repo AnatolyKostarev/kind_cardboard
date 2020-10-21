2f7018f7d70f4b21128b5006826891643927a30b
Anatoly Kostarev <anatolykostarev@gmail.com>
2020-10-21 15:52:48 +0300

    Homework 1.3 - подключен файл стилей слайдера slick.css

--- functions.php
+++ functions.php
@@ -3,4 +3,5 @@
 	function theme_name_scripts() {
 		wp_enqueue_style( 'style', get_stylesheet_uri(  ) );
 		wp_enqueue_style( 'animate', get_template_directory_uri(  ) . './assets/js/animate.css' );
+		wp_enqueue_style( 'slick', get_template_directory_uri(  ) . './assets/slick/slick.css' );
 	}
